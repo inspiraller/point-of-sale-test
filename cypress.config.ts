e25a5e55257b9eb2a50218f69f9832c0ec846b7c
stevetomlin <steveltomlin@gmail.com>
2024-07-09 16:37:28 +0100

    Fix - creation of coverage folder. Must be setupNodeEvents inside cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "cypress";
-import codeCover from "@cypress/code-coverage/task";
+import codeCover from "@cypress/code-coverage/task"; // if require doesn't work, just import it
 export default defineConfig({
   env: {
     codeCoverage: {
@@ -8,13 +8,19 @@ export default defineConfig({
   },
   e2e: {
     baseUrl: "http://localhost:3000",
-
+    setupNodeEvents(on, config) {
+      codeCover(on, config);
+      return config;
+    },
   },
   component: {
     devServer: {
       framework: "react",
       bundler: "vite",
     },
-
+    setupNodeEvents(on, config) {
+      codeCover(on, config);
+      return config;
+    },
   },
 });
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,6 @@
+const injectDevServer = require("@cypress/react/plugins/next");
 module.exports = (on, config) => {
+  injectDevServer(on, config);
   require('@cypress/code-coverage/task')(on, config);
   return config;
 };
